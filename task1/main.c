ff3a3a5cd3b174806783fb77ac022444972cd710
SergejGavrilov <sergey.gavrilov97@mail.ru>
2016-02-20 12:36:03 +0300

    create

---
+++ task1/main.c
@@ -0,0 +1,40 @@
++#include <stdio.h>
+ +#include <stdlib.h>
+ +
+ +int main()
+ +{
+ +    int n, x1, y1, x2, y2, i, j;
+ +    int** a;
+ +    scanf("%d", &n);
+ +
+ +
+ +
+ +
+ +    a = (int**)malloc((n + 1) * sizeof(int*));
+ +    for (i = 1; i <= n; i++){
+ +        a[i] = (int*)malloc((n + 1) * sizeof(int));
+ +    }
+ +    while (1){
+ +            scanf("%d", &x1);
+ +            if (x1 == 0){
+ +                break;
+ +            }
+ +            scanf("%d %d %d", &y1, &x2, &y2);
+ +
+ +        for (i = 1; i <= n; i++){
+ +            for (j = 1; j <= n; j++){
+ +                a[i][j] = i * j;
+ +            }
+ +        }
+ +        for (i = y1; i <= y2; i++, printf("\n"))
+ +            for (j = x1; j <= x2; j++)
+ +                printf("%*d",7, a[i][j]);
+ +    }
+ +    for (i = 1; i <= n; i++){
+ +        free(a[i]);
+ +        a[i] = NULL;
+ +    }
+ +    free(a);
+ +    a = NULL;
+ +    return 0;
+ +}
