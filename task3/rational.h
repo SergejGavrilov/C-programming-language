a797f4e18fe91db0406fb50457f3cd6eae3e4344
SergejGavrilov <sergey.gavrilov97@mail.ru>
2016-03-07 16:28:49 +0300

    Update rational.h

--- task3/rational.h
+++ task3/rational.h
@@ -5,13 +5,13 @@
 class rational{
 	private:
 		int n, d;
-		int gcd(int a, int b);
-        int lcm(int a, int b);
+		int gcd(int a, int b) const;
+        int lcm(int a, int b) const;
 
 	public:
 	    rational();
-		rational(int q);
-		rational(int n, int d);
+		rational(int);
+		rational(int, int);
 		int getNum() const;
 		int getDenom() const;
 		rational operator+(rational const &)const;
