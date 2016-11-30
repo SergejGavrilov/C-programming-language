375de39d506f20deb1c79871d52ee6f64cccf2ec
SergejGavrilov <sergey.gavrilov97@mail.ru>
2016-11-30 16:11:37 +0300

    Update main.c

--- task2/main.c
+++ task2/main.c
@@ -1,164 +1,267 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 
-typedef struct person{
-    char name[50];
-    char tel[20];
-    char checkname[50], chektel[20];
+typedef struct {
+    char *name;
+    char *phone;
     int id;
-};
+} Person;
 
-typedef struct vector{
-    struct person *p;
-    int vsize;
-    int currentsize;
-};
+typedef struct {
+    Person *people;
+    int length;
+    int lasID;
+} Notebook;
 
-void toLowerCase(char* s){
-    int i = 0;
-    for (i = 0; i < strlen(s); i++){
-        if (s[i] >= 'A' && s[i] <= 'Z')
-			s[i] -= 'A' - 'a';
-    }
+Notebook book;
 
-}
-void cleanString(char* str)
-{
-    int i;
-    for (i = 0; i < strlen(str); i++){
-        str[i] = NULL;
-    }
-}
+char *name_of_file;
 
-void read(char* first, char* second)  //функция читает телефон и записывает его Эбез шедухи"
-{
-    char c;
-    int k;
-    int i = 0;
-    int j = 0;
-    cleanString(first);
-    cleanString(second);
-    while ((c = getchar()) == ' '){}
-    while (c != '\n')
-    {
-        first[i++] = c;
-        if (c != '(' && c  != ')' && c != ' ' && c != '-')
-        {
-            second[j++] = c;
-        }
-        c = getchar();
-    }
-    for (k = i; k < strlen(first); k++) // При чтении образуется мусор, который нуэно уюирать
-    {
-        first[k] = NULL;
-    }
-    for (k = j; k < strlen(second); k++)  // При чтении образуется мусор, который нуэно уюирать
-    {
-        second[k] = NULL;
-    }
-            /*int i, j = 0;
-    for (i = 0; i < strlen(first); i++){
-        if (first[i] != '(' && first[i]  != ')' && first[i]  != ' ' && first[i] != '-'){
-            second[j++] = first[i];
-
-        }
-    }
-    for (i = j; j < strlen(second); j++){
-        second[i] = NULL;
-    }*/
-}
-
-void vecInit(struct vector *vec){
-    vec->p = (struct person*)malloc(10 * sizeof(struct person));
-    vec->vsize = 10;
-    vec->currentsize = 0;
-
-}
-
-void vecAdd(struct vector* vec, struct person per){
-    if (vec->currentsize == vec->vsize)
-    {
-        vec->p = (struct vector*) raelloc(vec->p, (vec->vsize * 2) * sizeof(struct vector));  //когда место в вектора закачивается, расширяем его в 2 раза
-    }
-    vec->currentsize += 1;
-    strcpy(vec->p[vec->currentsize].name, per.name);
-    strcpy(vec->p[vec->currentsize].checkname, per.checkname);
-    strcpy(vec->p[vec->currentsize].tel, per.tel);
-    strcpy(vec->p[vec->currentsize].chektel, per.chektel);
-    strcpy(vec->p[vec->currentsize].id, per.id);
-}
-
-int main(int argc, char *argv[])
-{
-    struct person per;
-    struct vector book;
-    char command[30];
-    char name[50];
-    char tel[20];
-    int j, i, currentID = 0;
-    FILE *file;
-    vecInit(&book);
-
-    if (file == NULL)
-    {
-
-        printf("new file created \n");
-    }else
-    {
-        file = fopen(argv[1], "r");
-        while (!EOF)
-        {
-
-            fscanf(file, "%d %s",  per.id, per.name);
-            read(per.tel, per.chektel);
-            strcpy(per.checkname, per.name);
-            toLowerCase(per.checkname);
-            currentID = per.id + 1;
-            vecAdd(&book, per);
-        }
-    }
-    file = fopen(argv[1], "w");
-
-
-    while(1){
-        scanf("%s", command);
-        if (!strcmp(command, "create")){
-
-            scanf("%s \n", per.name);
-            strcpy(per.checkname, per.name);
-            toLowerCase(per.checkname);
-            read(per.tel, per.chektel);
-            currentID += 1;
-            fprintf(file, "%d %s %s \n", currentID, per.name, per.tel);
-            printf("%d %s %s %s %s \n", currentID, per.name, per.checkname, per.tel, per.chektel);
-
-
-        }else if(!strcmp(command, "exit")){
-            //for (j = 0; j <= i; j++){
-                //printf("%s %s \n", book.p[j].name, book.p[j].tel);
-            //}
-            break;
-         }else if (!strcmp(command, "copy")){
-            scanf("%s %s", per.name, per.tel);
-            strcpy(per.checkname, per.name);
-            printf("%d \n", strlen(per.tel));
-            toLowerCase(per.checkname);
-            printf("%s \n", per.checkname);
-        }else if (!strcmp(command, "tr")){
-
-            read(per.tel, per.chektel);
-            printf("%s %s \n", per.tel, per.chektel);
-        }else if (!strcmp(command, "print"))
-        {
-            for (i = 0; i < book.currentsize; i++)
-            {
-                printf("%d %s %s %s %s \n",book.p[i].id, book.p[i].name,
-                        book.p[i].checkname, book.p[i].tel, book.p[i].chektel);
+char *getname(FILE *file) {
+    char *name = NULL;
+    int current_len = 0;
+    int c = fgetc(file);
+    int size = 1;
+    while (c != EOF) {
+        if (!isspace(c)) {
+            if (isalpha(c)) {
+                if (current_len + 1 == size) {
+                    size *= 2;
+                    name = realloc(name, size * sizeof(char));
+                }
+                name[current_len++] = tolower(c);
+                name[current_len] = '\0';
+            } else {
+                printf("Invalid name\n");
+                fflush(file);
+                return NULL;
             }
+        } else if (current_len > 0) break;
+        c = fgetc(file);
+    }
+    return name;
+}
+
+
+char *getphone(FILE *file) {
+    char *phone = NULL;
+    int current_len = 0;
+    int c = fgetc(file);
+    int size = 1;
+    int brackets = 2;   //only two brackets allowed once in a phone number
+    while (c != EOF) {
+        if (!isspace(c)) {
+            if (isdigit(c)) {
+                if (current_len + 1 == size) {
+                    size *= 2;
+                    phone = realloc(phone, size * sizeof(char));
+                }
+                phone[current_len++] = c;
+                phone[current_len] = '\0';
+            } else if ((current_len > 0 && c == '+') || (brackets != 2 && c == '(') || (brackets != 1 && c == ')') ||
+                       (current_len == 0 && c != '+') || (c == '-' && phone[current_len - 2] == '-')) {
+                printf("Invalid phone\n");
+                fflush(stdout);
+                return NULL;
+            }
+            else if (c == '(')
+                brackets = 1;
+            else if (c == ')')
+                brackets = 0;
+        } else if (current_len > 0) break;
+        c = fgetc(file);
+    }
+    return phone;
+}
+
+void writeToFile() {
+    FILE *file = fopen(name_of_file, "w");
+    int i;
+    for (i = 0; i < book.length; i++) {
+        fprintf(file, "%d %s %s\n", book.people[i].id, book.people[i].name, book.people[i].phone);
+        fflush(file);
+    }
+    fclose(file);
+}
+
+
+void create(FILE *file, int id) {
+    char *name = getname(file);
+    char *phone = getphone(file);
+    if (name == NULL || phone == NULL) {
+        printf("unable to create\n");
+        fflush(stdout);
+        return;
+    }
+    if (id > book.lasID) book.lasID = id;
+    book.people = (Person *) realloc(book.people, (book.length + 1) * sizeof(Person));
+    book.people[book.length].name = name;
+    book.people[book.length].phone = phone;
+    book.people[book.length].id = id;
+    book.length += 1;
+    printf("successfully created\n");
+    fflush(stdout);
+    writeToFile();
+
+}
+
+/*void create(FILE *file, int id) {
+    char *name = getname(file);
+    char *phone = getphone(file);
+    if (id > book.lasID) book.lasID = id;
+    book.people = (Person *) realloc(book.people, (book.length + 1) * sizeof(Person));
+    book.people[book.length].name = name;
+    book.people[book.length].phone = phone;
+    book.people[book.length].id = id;
+    book.length += 1;
+    writeToFile();
+
+}
+*/
+int issubstr(char *str1, char *str2) {
+    return (int) (strstr(str1, str2) != NULL);
+}
+
+void find(FILE *file) {
+    int c = fgetc(file);
+    while (c != EOF && isspace(c)) {
+        c = fgetc(file);
+    }
+    ungetc(c, file);
+    char *token;
+    if (isalpha(c)) {
+        token = getname(file);
+    } else {
+        token = getphone(file);
+    }
+    if (token == NULL){
+        printf("Unable to find");
+        fflush(file);
+        return;
+    }
+    int i = 0;
+    int found = 0;
+    for (i = 0; i < book.length; i++) {
+        if (issubstr(book.people[i].name, token) || (!strcmp(token, book.people[i].phone))) {
+            printf("%d %s %s\n", book.people[i].id, book.people[i].name, book.people[i].phone);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        printf("couldn't find");
+    fflush(stdout);
+}
+
+int getID() {
+    FILE *file = stdin;
+    int c = fgetc(file);
+    while (c != EOF && isspace(c)) {
+        c = fgetc(file);
+    }
+    if (isdigit(c)) {
+        ungetc(c, file);
+        scanf("%d", &c);
+        return c;
+    }
+    return -1;
+}
+
+void delete() {
+    int id = getID();
+    int found = 0;
+    int i = 0;
+    for (i = 0; i < book.length; i++) {
+        if (book.people[i].id == id) {
+            found = 1;
+            book.length--;
+            free(book.people[i].phone);
+            free(book.people[i].name);
+            /*book.people[i].id = book.people[book.length].id;
+            book.people[i].name = book.people[book.length].name;
+            book.people[i].phone = book.people[book.length].phone;
+            */
+            book.people[i].id = book.people[book.length].id;
+            book.people[i].name = book.people[book.length].name;
+            book.people[i].phone = book.people[book.length].phone;
+            book.people = (Person *)realloc(book.people, book.length * sizeof(Person));
+            break;
         }
 
+    }
+    if (found == 0)
+        printf("Unable to find ID\n");
+    else
+        printf("successfully deleted\n");
+
+    fflush(stdout);
+    writeToFile();
+}
+
+void change() {
+    int id = getID();
+    char type[6];
+    int c = fgetc(stdin);
+    while (c != EOF && isspace(c)) {
+        c = fgetc(stdin);
+    }
+    if (isalpha(c)) {
+        ungetc(c, stdin);
+        scanf("%s", type);
+    }
+    int i = 0;
+    for (i = 0; i < book.length; i++) {
+        if (book.people[i].id == id) {
+            char *token;
+            if (!strcmp(type, "number")) {
+                token = getphone(stdin);
+                book.people[i].phone = token;
+            }
+            else {
+                token = getname(stdin);
+                book.people[i].name = token;
+            }
+            writeToFile();
+            break;
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+    name_of_file = (argc > 1) ? argv[1] : "book.txt";
+    FILE *file = fopen(name_of_file, "a+");
+    if (file == NULL) {
+        printf("Error: could not open file %s \n", name_of_file);
+        fflush(stdout);
+    }
+    rewind(file);
+
+    int id;
+    book.lasID = 0;
+    book.people = NULL;
+    book.length = 0;
+    while (fscanf(file, "%d", &id) != EOF) {
+        create(file, id);
+    }
+    fclose(file);
+    char cmd[6];
+    while (1) {
+        scanf("%s", cmd);
+        if (!strcmp(cmd, "find")) {
+            find(stdin);
+        } else if (!strcmp(cmd, "create")) {
+            create(stdin, book.lasID + 1);
+        } else if (!strcmp(cmd, "delete")) {
+            delete();
+        } else if(!strcmp(cmd, "change")) {
+            change();
+        } else if (!strcmp(cmd, "exit")) {
+            break;
+        }
     }
     return 0;
 }
